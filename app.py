36ebd39fbf795991a36456b8b79682d6582ae77e
Priyansh Patel <priyanshpatel741@gmail.com>
2025-08-10 10:47:12 +0530

    change in app.py

--- app.py
+++ app.py
@@ -59,5 +59,9 @@ def result_page():
     else:
         return redirect(url_for("prediction"))
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT as env variable
+    app.run(host='0.0.0.0', port=port)    
